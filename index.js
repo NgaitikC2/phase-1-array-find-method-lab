d83e9181547988014a8f40a6f44e4f8f48fc3276
Ngai Tik Chung <NgaitikK.Chung94@gmail.com>
2023-10-28 22:59:39 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+
+// const record = [
+//     {year: "2023", result: "W"},
+//     {year: "2024", result: "N/A"},
+//     {year: "2025", result: "L"}
+// ]
+
+// function superbowlWin() {
+//     ['year', 'result']
+// }
+
+
+function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === "W");
+
+    if (winningGame) {
+        return winningGame.year;
+    } else {
+        return undefined;
+    }
+}
